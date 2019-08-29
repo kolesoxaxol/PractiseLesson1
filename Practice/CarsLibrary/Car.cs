dc9529b390989c969972b9e078cd1ccd09f202e3
VasylTravin <travin.vasilii@gmail.com>
2019-08-29 23:47:33 +0300

    First build

---
+++ Practice/CarsLibrary/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsLibrary
+{
+    public class Car
+    {
+        public int discount;
+        public Car(string name, int price, string color)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Color = color;
+        }
+
+        public string Name { get; }
+        public int Price { get; }
+        public string Color { get; }
+
+
+    }
+}
