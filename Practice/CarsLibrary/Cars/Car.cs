438bea7db29ad4500e21a057546e321b70862854
VasylTravin <travin.vasilii@gmail.com>
2019-09-03 02:01:57 +0300

    Created 3 classes for cars with implemented encapsulation

---
+++ Practice/CarsLibrary/Cars/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsManagement.Cars
+{
+    public abstract class Car
+    {
+        private int _discountPrice;
+
+        public Car(string name, int price, string color)
+        {
+            this.Name = name;
+            this.Price = price;
+            this.Color = color;
+        }
+
+        public string Name { get; internal set; }
+        public string Color { get; internal set; }
+        public int Price { get; internal set; }
+        public int DiscountPrice
+        {
+            get
+            {
+                return _discountPrice;
+            }
+            internal set
+            {
+                int discount = value;
+
+                int newPrice = this.Price*(1 - discount/100);
+
+                bool discountTooBig =  newPrice < this.Price * 0.7;
+
+                if(discountTooBig)
+                {
+                    Console.WriteLine("This discount is too big! Price is set to default.");
+                    _discountPrice = this.Price;
+                }
+                else
+                {
+                    _discountPrice = newPrice;
+                }
+
+            }
+        }
+
+    }
+}
---
+++ Practice/CarsLibrary/Cars/Truck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsManagement.Cars
+{
+    public class Truck : Car
+    {
+
+        public Truck(string name, int price, string color) : base(name, price, color)
+        {
+        }
+
+        public new int DiscountPrice
+        {
+            get
+            {
+                DateTime date = new DateTime();
+                return this.Price*(1 - 2 * date.Month / 100);
+            }
+        }
+    }
+}
