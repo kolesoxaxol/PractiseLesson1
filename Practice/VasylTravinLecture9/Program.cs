a63c95fb856269237cd8705234ec9026ed1144c6
VasylTravin <travin.vasilii@gmail.com>
2019-09-03 02:14:23 +0300

    final fix in program. Now I see - if I do any changes I have to check every single file

--- Practice/VasylTravinLecture9/Program.cs
+++ Practice/VasylTravinLecture9/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CarsLibrary;
+using CarsManagement;
 
 namespace VasylTravinLecture9
 {
@@ -13,39 +13,7 @@ namespace VasylTravinLecture9
         {
             Console.WriteLine("Welcome! Please, define 3 cars!");
 
-            //generating library of 3 cars;
-            int cycleInd = 0;
-            while(cycleInd<3)
-            {
-                Console.WriteLine("Name of a car:");
-                string name = Console.ReadLine();
-                Console.WriteLine("Price of a car:");
-                int price = int.Parse(Console.ReadLine());
-                Console.WriteLine("Color:");
-                string color = Console.ReadLine();
-                var tempCar = new Car(name, price, color);
-                CarStock.AddCarr(tempCar);
-                cycleInd++;
-            }
-
-            //showing all cars;
-
-            CarStock.ShowCars();
-
-            //giving discount;
-
-            Console.WriteLine("\nProvide an exact discount:");
-            int disc = int.Parse(Console.ReadLine());
-            Console.WriteLine("What car from the list do you want to buy with this discount? Please, select \"N\" from the list.");
-            int carNum = int.Parse(Console.ReadLine());
-            Car discCar = CarStock.Discount(carNum, disc);
-            Console.WriteLine($"You sell the car of model {discCar.Name}, of color {discCar.Color} with a price of {discCar.Price}. Original price is {CarStock.GetCars()[carNum].Price}.");
-
-            Console.WriteLine("\nTesting for changes in original CarStock list.\n");
-            CarStock.ShowCars();
-
-            //testing for manual changes of car's price in original CarStock;
-            //CarStock.GetCars()[1].Price = 100;
+            
 
             Console.ReadKey();
         }
