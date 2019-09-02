b56dd9ed0b0fad277fe94cd60ad9ddce13a0cf5e
Kurilo Dmitry <Demro1@yandex.ru>
2019-09-02 21:54:08 +0300

    Adding 2 new classes Minivan & truck

---
+++ Car/Truck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+	public class Truck : CarCreator
+	{
+		public int CarryWeight { get; set; }
+		public bool Warranty { get; set; }
+		public int Mileage { get; set; }
+
+		public Truck(string name, string color, int price, int weight, bool warranty, int mileage) : base (name, color, price)
+		{
+			CarryWeight = weight;
+			Warranty = warranty;
+			Mileage = mileage;
+			discount = 30;
+		}
+
+	}
+}
